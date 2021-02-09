bcebb8ffab45206c57f20712386823267fc27785
Jose Martinez <jmartinez@and.digital>
2021-02-09 19:37:33 +0000

    testing ai on the cloud

--- pages/api/server.js
+++ pages/api/server.js
@@ -8,14 +8,52 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
-module.exports = (req, res) => {
-  const data = req.body;
-  console.log(data);
+const dialogflow = require("@google-cloud/dialogflow");
+const uuid = require("uuid");
 
+module.exports = async (req, res) => {
+  const data = req.body;
+  console.log("data", data);
+
+  const sessionId = uuid.v4();
+
+  // Create a new session
+  const sessionClient = new dialogflow.SessionsClient();
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    "newagent-siox",
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: data,
+        // The language used by the client (en-US)
+        languageCode: "en-US",
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+  if (result.intent) {
+    console.log(`  Intent: ${result.intent.displayName}`);
+  } else {
+    console.log(`  No intent matched.`);
+  }
+
+  console.log("end");
   return new Promise(() => {
     pusher
       .trigger("my-channel", "my-event", {
-        message: data,
+        message: result.fulfillmentText,
       })
       .then(() => {
         return res.status(200).end("sent event successfully");
--- utils/utils.js
+++ utils/utils.js
@@ -5,3 +5,10 @@ export const initSpeechRecognition = () => {
   //   const socketIo = require("socket.io");
   return recognition;
 };
+
+export const synthVoice = (text) => {
+  const synth = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance();
+  utterance.text = text;
+  synth.speak(utterance);
+};
